2435c1a4a9f13818f9de6c566d9f996cb900ed3b
Parker Nix <71665527+ParkerNix@users.noreply.github.com>
2022-02-20 22:31:03 -0500

    Java array initial

--- beta/menu_listing.js
+++ beta/menu_listing.js
@@ -11,9 +11,11 @@ for (let i = 0; i < foods.length; i++) {
     let foodArray = foods[i];
 
     let foodImage = foodArray[0];
+    let foodPrice = foodArray[1];
+    let foodName = foodArray[2];
     console.log('foodImage is ' + foodImage);
 
-    text += '<img src="' + foodImage + '">';
+    text += '<div class="menuitem"> <a href="edit_item.html"> <img src="' +  foodImage + '" alt="cheesesteak"> </a> <h3>' + foodName + '</h3> </div>';
 }
 
 document.getElementById("demo").innerHTML = text;
