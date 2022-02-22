5d0f05fcedc7207e72c0961b183bf31187f74333
Parker Nix <71665527+ParkerNix@users.noreply.github.com>
2022-02-22 10:54:38 -0500

    Array update

--- beta/menu_listing.js
+++ beta/menu_listing.js
@@ -1,9 +1,14 @@
 let foods = [
     ['images/cheesesteak.png', '6.00', 'Cheesesteak'],
-    ['images/cheesesteak.png', '6.00', 'Cheesesteak'],
-    ['images/cheesesteak.png', '6.00', 'Cheesesteak'],
-    ['images/cheesesteak.png', '6.00', 'Cheesesteak'],
-    ['images/cheesesteak.png', '6.00', 'Cheesesteak'],
+    ['images/bacon_cheeseburger.png', '6.00', 'Bacon Cheeseburger'],
+    ['images/turkey_club.png', '6.00', 'Turkey Club'],
+    ['images/hot_sausage.png', '6.00', 'Hot Sausage'],
+    ['images/buffalo_cheesestake.png', '6.00', 'Buffalo Cheesesteak'],
+    ['images/ham_club.png', '6.00', 'Ham Club'],
+    ['images/grilled_chicken_hoagie.png', '6.00', 'Grilled Chicken Hoagie'],
+    ['images/lamb_gyro.png', '6.00', 'Lamb Gyro'],
+    ['images/mushroom_cheesesteak.png', '6.00', 'Mushroom Cheesesteak'],
+    ['images/hot_dog.png', '6.00', 'Hot Dog']
 ];
 
 let text = "";
@@ -15,7 +20,7 @@ for (let i = 0; i < foods.length; i++) {
     let foodName = foodArray[2];
     console.log('foodImage is ' + foodImage);
 
-    text += '<div class="menuitem"> <a href="edit_item.html"> <img src="' +  foodImage + '" alt="cheesesteak"> </a> <h3>' + foodName + '</h3> </div>';
+    text += '<div class="menuitem"> <a href="edit_item.html"> <img src="' +  foodImage + '" alt="' + foodName + '"> <span class="pricetag lunchtag">$' + foodPrice + '</span> </a> <h3>' + foodName + '</h3> </div>';
 }
 
 document.getElementById("demo").innerHTML = text;
