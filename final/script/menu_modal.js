5d2beac6ae6d6069b741b849490f3a84136295a3
alliedrake <71614898+alliedrake@users.noreply.github.com>
2022-03-02 18:22:55 -0500

    Modal integration

---
+++ final/script/menu_modal.js
@@ -0,0 +1,14 @@
+document.getElementById('modal-switch').addEventListener('click', function() {
+    document.getElementById('overlay').classList.add('is-visible');
+    document.getElementById('modal').classList.add('is-visible');
+  });
+  
+  document.getElementById('close-btn').addEventListener('click', function() {
+    document.getElementById('overlay').classList.remove('is-visible');
+    document.getElementById('modal').classList.remove('is-visible');
+  });
+  
+  document.getElementById('overlay').addEventListener('click', function() {
+    document.getElementById('overlay').classList.remove('is-visible');
+    document.getElementById('modal').classList.remove('is-visible');
+  });
