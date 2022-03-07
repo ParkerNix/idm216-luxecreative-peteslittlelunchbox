12cbf96648488dd29d018b2a2a46664d290a1ecb
alliedrake <71614898+alliedrake@users.noreply.github.com>
2022-03-07 17:02:59 -0500

    Added heart to menu page

---
+++ final/script/heart.js
@@ -0,0 +1,5 @@
+let jsheartno = document.getElementsByClassName('js-heart');
+let jsheart = jsheartno[0];
+
+jsheart.addEventListener('click', function () {
+jsheart.classList.toggle('is-active'); }, false);
