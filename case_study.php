a8f0e1b4de2ae908644bee068c4dd4d4d5701327
Parker Nix <71665527+ParkerNix@users.noreply.github.com>
2022-03-19 13:58:58 -0400

    Fixing some cart stuff, small changes

---
+++ case_study.php
@@ -0,0 +1,95 @@
+<head>
+    <link rel= "stylesheet" href= "css/main.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <title>Case Study | Recipe & Me</title>
+</head>
+<body>
+    <div class="casebody">
+        <h1>Case Study</h1>
+        <div class="section">
+            <h2>Overview</h2>
+            <p>In this project, I was to create a recipe website that uses PHP as the main scripting language in order 
+                to learn more about server-side development and working with databases. After looking at other recipe pages, 
+                I aimed to make mine simple and breathable, without all the extra things that recipe websites tend to put 
+                on their pages. To accomplish this, I designed the site as a prototype first, then coded out a static 
+                version of it. After this, I applied PHP code to my site that allowed users to add, view, edit, and delete 
+                recipes. Finally, I added in more recipes and did some finishing touches (Like adding more recipes) before 
+                calling it done. The end product was a success as it had all the functionality that I had originally 
+                intended for the site to have and it was simple and breathable.</p>
+        </div>
+        <div class="section">
+            <h2>Context and Challenges</h2>
+            <p class="case">For this project, I had to create a recipe site that functioned with PHP as the main scripting 
+                language. The purpose of which was to learn about the basics of server-side coding by making a larger site 
+                that generates pages based on a database.
+                My site was called Recipe & Me, and my intention for it was to be very simple and easy to use. Using other 
+                recipe websites, I’ve seen that a big problem is that they often have a lot of things on the page, which 
+                makes me feel very overwhelmed. Therefore, I wanted this focus on giving this site some breathing room 
+                between elements. My main goals were to have a system to add, view, edit, and delete recipes, use a simplified 
+                design for my site, and make sure everything on my page is functioning well.</p>
+        </div>
+        <div class="section">
+            <h2>Process and Insight</h2>
+            <div class="process">
+                <img class="images big" src="images/figma.png" alt="Figma prototype">
+                <p>To start, I designed a Figma file that would be the starting point for what my website would look like. I 
+                    used a tan for the main color on my site and used mostly greys for the rest of the page in order to keep 
+                    most of the color with the pictures of food that go with each recipe. I found this process to be pretty fun,
+                    as it was nice to just think about how to arrange things on the page without having to dig into the details 
+                    of how to code it.</p>
+            </div>
+            <div class="processRev">
+                <img class="images big" src="images/Alpha.png" alt="Alpha">
+                <p>Next, I worked on creating the static version of my site, without PHP. This wasn’t terribly difficult and it 
+                    was nice to refresh myself on how to use certain elements like flexbox and grid. One challenge that I had 
+                    here was coding with the intent of eventually adding in the PHP features. For the most part, I could just 
+                    take what I had from the design I made and code it out, but I was worried about setting it up in such a way 
+                    that I’d have to go back and re-code things out. To solve this, I looked into how coding PHP works in advance, 
+                    and I was even able to add in some basic PHP that just makes it easier to code in the header and footer for 
+                    each page.</p>
+            </div>
+            <div class="process">
+                <img class="images big" src="images/Beta.png" alt="Beta">
+                <p>After this, it was time to really get into PHP. I was really excited for this step because I was really looking 
+                    forward to learning more so I’d have experience when applying to co-ops. The first thing I did was connect my 
+                    site to a database that I created to store the information for each recipe. Then, I created the code that would 
+                    be used to add recipes to the database. These two steps were probably the hardest because they required a lot 
+                    of focus to understand why they weren’t working. However, this helped me on the rest of the steps because I knew 
+                    how to code each part in. After that I built in the functionality for viewing, editing, and deleting recipes. 
+                    This was really fun and satisfying to use once I was done.</p>
+            </div>
+            <div class="processRev">
+                <img class="images big" src="images/Final.png" alt="Final">
+                <p>Lastly, I needed to add in recipes, as well as putting in some finishing touches. I made the search function work
+                    and entered in data for a bunch of recipes. This step was really easy compared to connecting the database to my
+                    site, as there wasn't a lot to learn, just a lot of recipes to enter. Learning how to implement a search button was
+                    super helpful for me because it's a skill that I think a lot of websites need to have a great user experience.
+                    Without it, many pages would take forever to find if the site is too big.
+                </p>
+            </div>
+        </div>
+        <div class="section">
+            <h2>The Solution</h2>
+            <a href="http://parkerishere.com/IDM232"><p>Link to the final product</p></a>
+            <p>My final site opens to a homepage. “All recipes” takes you to a list of the recipes on the page, and clicking on one 
+                will show you more details on the recipe. There’s a button on the details page to edit an item, and clicking on that 
+                takes you to a page where you can edit the title, description, ingredients, and steps of each page. At the bottom is 
+                a button to delete recipes, which will permanently remove them. You can also add new recipes at the bottom of the page 
+                using the “New Recipe” button, and the page for that is about the same as the page for editing recipes.</p>
+        </div>
+        <div class="section">
+            <h2>Results</h2>
+            <p>In the end, I found that my project was successful. My page is capable of adding, viewing, editing, and deleting recipes 
+                and everything on my page functions as expected. In addition, it has a very simplified feel that offers the user a 
+                layout that doesn’t feel like it has no breathing room. Many of the family and friends that I showed the page to noted 
+                that the site felt very clean, but also inviting and warm. This indicates to me that I’ve been successful in my goals.
+                I also learned a lot in this process. Of course, I learned how to code in PHP and the interworkings of back-end 
+                development. This was the thing I was looking most forward to when I began this project, and I feel like I’ve gained a 
+                lot of knowledge on it. However, I also learned a lot about how to manage time when learning something new. Learning how 
+                to implement the database in my site took a good amount of time to learn, and I felt like I had to rush a bit to finish 
+                because of it. Next time, I’ll be more generous with how long I give myself to fully understand a concept in respect to 
+                the deadline I have to finish the project I need to apply it to.</p>
+        </div>
+    </div>
+</body>
--- final/script/pickup.js
+++ final/script/pickup.js
@@ -1,3 +1,5 @@
 if (localStorage.getItem("pickuptime")) {
     document.getElementById("pickuptime").innerHTML = localStorage.getItem("pickuptime");
     }
+
+    localStorage.removeItem("chosenitem1");
